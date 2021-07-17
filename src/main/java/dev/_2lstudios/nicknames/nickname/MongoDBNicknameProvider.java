9c842abafeff1bf421727c06a250f480d66540f4
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-07-17 18:32:47 -0300

    add incomplete mongodbnicknameprovider

---
+++ src/main/java/dev/_2lstudios/nicknames/nickname/MongoDBNicknameProvider.java
@@ -0,0 +1,15 @@
+package dev._2lstudios.nicknames.nickname;
+
+import java.util.UUID;
+
+public class MongoDBNicknameProvider implements NicknameProvider {
+    @Override
+    public String getNickname(UUID uuid) {
+        return null;
+    }
+
+    @Override
+    public String setNickname(UUID uuid, String nickname) {
+        return null;
+    }
+}
