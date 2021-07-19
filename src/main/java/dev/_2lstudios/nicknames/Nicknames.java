734b69ee02f4af6dee9c4157793ab9211ba0eb80
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-07-19 16:59:17 -0300

    update main plugin class

--- src/main/java/dev/_2lstudios/nicknames/Nicknames.java
+++ src/main/java/dev/_2lstudios/nicknames/Nicknames.java
@@ -3,9 +3,11 @@ package dev._2lstudios.nicknames;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import dev._2lstudios.nicknames.commands.NickCommand;
+import dev._2lstudios.nicknames.lang.LangManager;
 import dev._2lstudios.nicknames.listeners.PlayerJoinListener;
-import dev._2lstudios.nicknames.nickname.MongoDBNicknameProvider;
-import dev._2lstudios.nicknames.nickname.NicknameProvider;
+import dev._2lstudios.nicknames.nickname.providers.MongoDBNicknameProvider;
+import dev._2lstudios.nicknames.nickname.providers.NicknameProvider;
+import dev._2lstudios.nicknames.utils.ConfigUtil;
 
 public class Nicknames extends JavaPlugin {
     private static Nicknames instance;
@@ -18,10 +20,16 @@ public class Nicknames extends JavaPlugin {
     public void onEnable() {
         Nicknames.instance = this;
 
+        final ConfigUtil configUtil = new ConfigUtil(this);
+
+        configUtil.create("%datafolder%/config.yml", "config.yml");
+
         final NicknameProvider nicknameProvider = new MongoDBNicknameProvider();
+        final NicknamesConfig nicknamesConfig = new NicknamesConfig(configUtil.get("%datafolder%/config.yml"));
+        final LangManager langManager = new LangManager(nicknamesConfig.getLang());
 
         this.getServer().getPluginManager().registerEvents(new PlayerJoinListener(this, nicknameProvider), this);
 
-        this.getCommand("nick").setExecutor(new NickCommand(this, nicknameProvider));
+        this.getCommand("nick").setExecutor(new NickCommand(this, nicknameProvider, langManager));
     }
 }
