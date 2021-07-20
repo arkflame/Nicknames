138d127929dcdb31ec64c44bf76a12d0c8273acc
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-07-20 16:43:06 -0300

    add extra validators

--- src/main/java/dev/_2lstudios/nicknames/validators/NicknameValidator.java
+++ src/main/java/dev/_2lstudios/nicknames/validators/NicknameValidator.java
@@ -2,14 +2,42 @@ package dev._2lstudios.nicknames.validators;
 
 import java.util.regex.Pattern;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Server;
+import org.bukkit.entity.Player;
 
 public class NicknameValidator {
     private static final Pattern pattern = Pattern.compile("[§a-zA-Z0-9_]*");
 
-    public static boolean isValid(final String nickname) {
+    public static boolean isValidLength(final String nickname) {
         final String strippedName = ChatColor.stripColor(nickname);
 
-        return strippedName.length() <= 16 && pattern.matcher(strippedName).matches();
+        return strippedName.length() <= 16 && strippedName.length() >= 3;
+    }
+
+    public static boolean isValidChars(final String nickname) {
+        final String strippedName = ChatColor.stripColor(nickname);
+
+        return pattern.matcher(strippedName).matches();
+    }
+
+    public static boolean isNotUsed(final String nickname) {
+        final String strippedName = ChatColor.stripColor(nickname);
+        final Server server = Bukkit.getServer();
+
+        for (final Player player : server.getOnlinePlayers()) {
+            if (strippedName.equals(player.getName())) {
+                return false;
+            } else {
+                final String strippedName1 = ChatColor.stripColor(player.getDisplayName());
+
+                if (strippedName.equals(strippedName1)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
